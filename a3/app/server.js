c082eb7bde890e42f75bca05c0380e2cf4e3655f
Kevin San Gabriel <kevinsangabriel@Kevins-MacBook-Pro-2.local>
2020-03-22 22:31:43 -0700

    added more cors

--- a3/app/server.js
+++ a3/app/server.js
@@ -27,10 +27,16 @@ const indexRouter = require('./routes/index')
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
+app.all('/', function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+})
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(cors());
 app.options('*', cors());
+
 
 
 
